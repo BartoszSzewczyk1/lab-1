a3109d5dd60412759009dd3cfc15a8c137cec592
Bartosz Szewczyk <b.szewczyk@pocztowy.pl>
2025-12-05 13:54:28 +0000

    2zajecia

--- main.tf
+++ main.tf
@@ -1,87 +1,28 @@
-variable "server-name" {
-  type        = string
-  default     = "kot"
-  description = "Name of a server to provision"
-}
-
-locals {
-  minNumberOfServer = 1
-  maxNumberOfServer = 10
-}
-
-locals {
-  countOfItems = {
-    disks = 13
-    servers = 22
-    max = local.maxNumberOfServer + var.number-of-disks
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.25.0"
     }
-}
-
-variable "number-of-servers" {
-  type        = number
-  description = "Required number of servers"
-  default     = 2
-  validation {
-    condition     = var.number-of-servers >= local.minNumberOfServer && var.number-of-servers < local.maxNumberOfServer
-    error_message = "Not supported number of server, it should be from the range [${local.minNumberOfServer}, ${local.maxNumberOfServer})"
   }
 }
 
-variable "number-of-disks" {
-  type = number
+provider "aws" {
+  # Configuration options
 }
 
-variable "list-of-names" {
-  type = list(string)
+resource "aws_s3_bucket" "my-bucket" {
+  bucket_prefix = "awsninja-1-12-"
 }
 
-output "result" {
-  value = "${var.number-of-servers} x ${var.number-of-servers}"
+output "bucket_name" {
+  value = aws_s3_bucket.my-bucket.bucket
 }
 
-output "number-of-resources" {
-  value = var.number-of-disks + var.number-of-servers
-}
+resource "aws_s3_object" "object" {
+  for_each = fileset(path.module, "messages/*")
 
-output "list-of-names" {
-  value = "${join(", ", var.list-of-names)}"
-}
-
-output "key-from-map" {
-  value = lookup(local.countOfItems, "max")
-}
-
-
-
-
-# Write a calculator
-
-variable "a" {
-  type = number
-}
-
-variable "b" {
-  type = number
-}
-
-locals {
-  calc = {
-    "+" = var.a + var.b
-    "-" = var.a - var.b
-    "*" = var.a * var.b
-    "/" = var.b != 0 ? var.a / var.b : "ERROR: Division by zero"
-  }
-}
-
-# Allowed: +, -, *, /
-variable "operand" {
-  type = string
-  validation {
-    error_message = "Operand must be: + - * or /"
-    condition = contains(["+", "-", "*", "/"], var.operand)
-  }
-}
-
-output "result" {
-  value = local.calc[var.operand]
+  bucket = aws_s3_bucket.my-bucket.bucket
+  key    = basename(each.key)
+  source = each.key
 }
