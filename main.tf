219ff5c1cb173e9a90a9bf4dda5fdd64da317388
Bartosz Szewczyk <b.szewczyk@pocztowy.pl>
2025-12-08 09:20:20 +0000

    2nd_homework

--- main.tf
+++ main.tf
@@ -31,3 +31,35 @@ resource "aws_s3_object" "object" {
 # Input - Tagi, gdzie domyslne jest w variable "purpouse": "learning" (moze byc nadpisany)
 # Zawsze dodawany tag do bucketu (lub wszedzie) "owner": "jw"
 # Outputs: bucket ARN oraz pełny URL z HTTP
+
+variable "bucket_prefix" {
+  type = string
+  default = "awsninja-1-12"
+}
+
+resource "random_string" "sufiks" {
+  length  = 12
+  special = false
+}
+
+variable "purpouse" {
+  type = string
+  default = "learning"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.bucket_prefix}-${random_string.sufiks.result}"
+
+  tags = {
+    purpouse = var.purpouse
+    owner = "awsninja-1-12"
+  }
+}
+
+output "bucket_ARN" {
+  value = aws_s3.bucket.ARN
+}
+
+output "bucket_URL" {
+  value = "http://${aws_s3_bucket.bucket.bucket}.s3.amazonaws.com"
+}
