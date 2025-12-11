9ff345f586027801c948f5fcc7f32aa96dc2a6ec
Bartosz Szewczyk <b.szewczyk@pocztowy.pl>
2025-12-11 08:46:08 +0000

    homework3

--- main.tf
+++ main.tf
@@ -16,3 +16,41 @@ resource "aws_s3_object" "object" {
   key    = basename(each.key)
   source = each.key
 }
+
+
+# INPUT
+# cidr ="10.0.0.0/16" (string)
+# public_ip_on_launch = true|false (bool)
+# subnets = ["10.0.1.0/24", "10.0.2.0/24"] # subnety prywatne
+
+# tags
+
+# output
+# vpc_id
+
+resource "aws_vpc" "my-vpc" {
+  cidr_block = var.cidr
+
+  tags = merge(
+    {
+      name = "my-vpc"
+    },
+    var.tags
+  )
+}
+
+resource "aws_subnet" "private" {
+  for_each = toset(var.subnets)
+
+  vpc_id = aws_vpc.my-vpc.id
+  cidr_block = each.key
+  map_public_ip_on_launch = var.public_ip_on_launch
+
+  tags = merge(
+    {
+      name = "private-${replace(each.key, "/", "-")}"
+      type = "private"
+    },
+    var.tags
+  )
+}
--- variables.tf
+++ variables.tf
@@ -7,3 +7,30 @@ variable "tags-to-ignore" {
   type    = list(string)
   default = ["department"]
 }
+
+# homewaork 3
+
+variable "cidr" {
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_ip_on_launch" {
+  type = bool
+  default = false
+}
+
+variable "subnets" {
+  type = list(string)
+  default = [ 
+    "10.0.1.0/24",
+    "10.0.2.0/24" ]
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    "owner" = "awsninja12"
+    "purpose" = "learning"
+  }
+}
